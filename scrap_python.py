9288295f741ad8051cea904760407e381c357b83
Caroline <62635799+LifAlpha@users.noreply.github.com>
2020-03-26 19:08:50 -0700

    Create scrap_python.py
    Added general program intro and intentions

---
+++ scrap_python.py
@@ -0,0 +1,15 @@
+import os, time
+
+#program introduces itself
+name = input("Hi...\n")
+time.sleep(2)
+print("\nHi " + name + "!\n")
+time.sleep(2)
+print("I store your scrapped code and comments so you can come back to them later.")
+time.sleep(2)
+print("I can also organize your scrap for you, or even delete what you don't want to see again.")
+print("Let's get started!\n")
+time.sleep(2)
+
+#program tells the user how to get started
+print("What would you like to do today? If you're not sure, just type 'What can you do?', and I'll explain.\n")
